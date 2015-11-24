e6cc9b3c65fe46a2fd7a6f842700fa66186f900b
Ph@ni <phaniapsr@gmail.com>
2015-11-24 15:56:34 +0530

    Removed comments

--- userController.js
+++ userController.js
@@ -5,17 +5,11 @@ var app = angular.module('userApp',[]);
 function userController($scope,$http,$location){
 	$scope.formData={};
 	$scope.createUser=function(){
-		/*$http.post('/api/createUser',$scope.formData).error(function(data){
-			console.log('Problem in creating user '+JSON.stringify(data));
-		});*/
 		$http.post('/api/createUser',$scope.formData).error(function(data){
 			
 			console.log('Problem in creating user '+JSON.stringify(data));
 		}).success(function(data){
 			console.log('User successfully created'+JSON.stringify(data));
-			//$window.location.href = '/profile';
-			//$http.get('/profile',data.userData);
-			//$location.url('/profile',data.userData);
 			$location.path('/profile');
 		});
 	}
